6828c696f49b43d07c390e79037318d2472fd16b
eddy-vera <122470243+eddy-vera@users.noreply.github.com>
2024-04-17 12:12:43 +0200

    fix: change default of output for existing groups

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,6 @@ output "groups" {
 output "groups_existing" {
   description = "Contains all resource groups considered existing based on global and local flags."
   value = var.use_existing_groups ? var.groups : {
-    for k, v in var.groups : k => v if lookup(v, "use_existing_group", false)
+    for k, v in var.groups : k => v if lookup(v, "use_existing_group", true)
   }
 }
