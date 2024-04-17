b6fb6cbe6338af0902aa9cbbf769438d9f77ea10
eddy-vera <122470243+eddy-vera@users.noreply.github.com>
2024-04-17 13:57:27 +0200

    fix: small correction output existing groups (#17)
    * fix: change default of output for existing groups
    
    * fix: Change default to "CanNotDelete" (with CamelCase)
    
    * fix: add override for name of the lock

--- main.tf
+++ main.tf
@@ -30,12 +30,12 @@ resource "azurerm_management_lock" "lock" {
     for k, v in var.groups : k => v if try(v.management_lock != null, false)
   }
 
-  name = "lock-${each.key}"
+  name = try(each.value.management_lock.name, "lock-${each.key}")
   scope = try(
     (var.use_existing_groups || lookup(each.value, "use_existing_group", false)) ? data.azurerm_resource_group.existing[each.key].id :
     azurerm_resource_group.groups[each.key].id, null
   )
 
-  lock_level = try(each.value.management_lock.level, "cannotdelete")
+  lock_level = try(each.value.management_lock.level, "CanNotDelete")
   notes      = try(each.value.management_lock.notes, null)
 }
