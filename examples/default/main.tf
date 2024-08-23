01a578680816274c8b7c5e7efe36c03aba413650
Jasper Post <jasper.post@cloudnation.nl>
2024-08-23 15:34:30 +0200

    Bump module versions to match new version

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -1,13 +1,13 @@
 module "naming" {
   source  = "cloudnationhq/naming/azure"
-  version = "~> 0.1"
+  version = "~> 0.13"
 
   suffix = ["demo", "dev"]
 }
 
 module "rg" {
   source  = "cloudnationhq/rg/azure"
-  version = "~> 0.1"
+  version = "~> 1.0"
 
   groups = {
     demo = {
