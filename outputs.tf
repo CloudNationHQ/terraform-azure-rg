c4d109af1a139c69fd0241e844ca24c8d7a924d2
Jasper Post <jasper.post@cloudnation.nl>
2024-08-27 11:22:23 +0200

    Format output.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "groups" {
   description = "Contains all resource groups, existing and new."
-  value = merge(azurerm_resource_group.groups, data.azurerm_resource_group.existing)
+  value       = merge(azurerm_resource_group.groups, data.azurerm_resource_group.existing)
 }
