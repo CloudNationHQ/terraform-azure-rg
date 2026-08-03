dbfb4a08ca9039998d7b62d482c86ac99bb39759
Jasper Post <jasper.post@cloudnation.nl>
2026-08-03 16:21:08 +0200

    Add default values back into variables.tf

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -9,10 +9,6 @@ module "rg" {
   source  = "cloudnationhq/rg/azure"
   version = "~> 3.0"
 
-  use_existing_groups = false
-  location            = null
-  tags                = { environment = "demo" }
-
   groups = {
     demo = {
       name     = module.naming.resource_group.name_unique
--- variables.tf
+++ variables.tf
@@ -31,14 +31,17 @@ variable "groups" {
 variable "use_existing_groups" {
   description = "use existing resource groups globally"
   type        = bool
+  default     = false
 }
 
 variable "location" {
   description = "default azure location to be used."
   type        = string
+  default     = null
 }
 
 variable "tags" {
   description = "tags to be added to the resources"
   type        = map(string)
+  default     = {}
 }
